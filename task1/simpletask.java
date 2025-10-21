7431630374de16997038d2df4250d31faf45393e
TexnoViktor <104459119+TexnoViktor@users.noreply.github.com>
2025-10-21 14:05:31 +0000

    add simpleTask class for task 1

---
+++ task1/simpletask.java
@@ -0,0 +1,26 @@
+package task1;
+
+public class simpleTask  implements Runnable{
+    private String taskName;
+    
+    public simpleTask(String Name){
+        this.taskName = Name;
+    }
+
+    @Override
+    public void run() {
+        // Jeder Thread führt diese Schleife aus 
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nachricht von " + taskName + ": Zähle " + i);
+
+            // Kurze Pause, um das "Interleaving" (Vermischen)
+            // wahrscheinlicher und besser sichtbar zu machen.
+            try {
+                Thread.sleep(100); // 100 Millisekunden warten
+            } catch (InterruptedException e) {
+                System.err.println(taskName + " wurde unterbrochen.");
+            }
+        }
+        System.out.println(taskName + " ist fertig.");
+    }
+}
